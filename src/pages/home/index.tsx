0d89549abb140424b3bc5da845f907a193044090
danangsptro <danangsaputro300@gmail.com>
2022-11-12 00:13:35 +0700

    config tailwind

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AppNavbar from "../../layouts/AppNavbar";
+
+function Index() {
+  return (
+    <div>
+      <AppNavbar />
+    </div>
+  );
+}
+
+export default Index;
